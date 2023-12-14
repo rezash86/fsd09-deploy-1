b30d414a662d8329d955be5b4491b67aad4e545c
rezash86 <shalchian.r@gmail.com>
2023-12-14 00:39:28 -0500

    Update MvcApplicationTests.java

--- src/test/java/com/jac/mvc/MvcApplicationTests.java
+++ src/test/java/com/jac/mvc/MvcApplicationTests.java
@@ -3,11 +3,7 @@ package com.jac.mvc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MvcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
